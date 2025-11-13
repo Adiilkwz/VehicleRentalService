cfe346056efa7979fea552aaed6a21e613507813
Adiilkwz <240692@astanait.edu.kz>
2025-11-13 17:29:36 +0500

    updated RentalRequestDTO

--- src/main/java/com/rental/model/RentalRequestDTO.java
+++ src/main/java/com/rental/model/RentalRequestDTO.java
@@ -1,31 +1,64 @@
 package com.rental.model;
 
-public class RentalRequestDTO {
-    private final String vehicleType;
-    private final int durationMinutes;
-    private final boolean addGps;
-    private final boolean addInsurance;
+import java.util.List;
 
-    public RentalRequestDTO(String vehicleType, int durationMinutes, boolean addGps, boolean addInsurance) {
-        this.vehicleType = vehicleType;
-        this.durationMinutes = durationMinutes;
-        this.addGps = addGps;
-        this.addInsurance = addInsurance;
+public class RentalRequestDTO {
+
+    public RentalRequestDTO() {
     }
+
+    private String vehicleType;
+    private int durationMinutes;
+    private boolean addGps;
+    private boolean addInsurance;
+    private UserDTO user;
+    private String paymentToken;
 
     public String getVehicleType() {
         return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
     }
 
     public int getDurationMinutes() {
         return durationMinutes;
     }
 
+    public void setDurationMinutes(int durationMinutes) {
+        this.durationMinutes = durationMinutes;
+    }
+
     public boolean isAddGps() {
         return addGps;
     }
 
+    public void setAddGps(boolean addGps) {
+        this.addGps = addGps;
+    }
+
     public boolean isAddInsurance() {
         return addInsurance;
+    }
+
+    public void setAddInsurance(boolean addInsurance) {
+        this.addInsurance = addInsurance;
+    }
+
+    public UserDTO getUser() {
+        return user;
+    }
+
+    public void setUser(UserDTO user) {
+        this.user = user;
+    }
+
+    public String getPaymentToken() {
+        return paymentToken;
+    }
+
+    public void setPaymentToken(String paymentToken) {
+        this.paymentToken = paymentToken;
     }
 }
